b0f7ff73e488464ba3a814011acb4273d92e16b0
taohonggou <cl889521@163.com>
2017-01-19 18:44:22 +0800

    新建了项目

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string  Index()
+        {
+            return "This is my default action... ";
+        }
+
+        // GET:/
+        public string Welcome()
+        {
+            return "This is my Welcome action...";
+        }
+    }
+}
